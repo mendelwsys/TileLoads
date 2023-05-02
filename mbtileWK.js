89432253008257a103b0c37fdb190b34cbd24e02
mendelwsys <120378833+mendelwsys@users.noreply.github.com>
2023-05-02 20:55:37 +0300

    mbtileCommit

--- mbtileWK.js
+++ mbtileWK.js
@@ -4,22 +4,57 @@ require('sqlite3').verbose();
 var tape = require('tape');
 var fs = require('fs');
 
+
 var MBTiles = require('@mapbox/mbtiles');
 
 var completed = { written: 0, read: 0 };
-new MBTiles(__dirname+'/mbtile/tstfile.mbtiles?mode=rwc', function(err, mbtiles)
+let mbTilePath=__dirname+'/mbtile/';
+if (!fs.existsSync(mbTilePath))
+    fs.mkdirSync(mbTilePath,{ recursive: true });
+
+new MBTiles(mbTilePath+'tstfile.mbtiles?mode=rwc', function(err, mbtiles)
 {
     completed.open = true;
+    completed.startFiles=0;
     if (err) throw err;
 
+    let pathPrefix=__dirname + '/RASTER_msk_/';
     mbtiles.startWriting(function (err) {
         completed.started = true;
         if (err) throw err;
+        fs.readdirSync(pathPrefix).forEach(insertFolder);
 
-        fs.readdirSync(__dirname + '/RASTER_msk_/15/').forEach(insertTile);
+        var myf=function ()
+        {
+            if (completed.written === completed.startFiles)
+            {
+                mbtiles.stopWriting(function (err)
+                {
+                    completed.stopped = true;
+                    if (err) throw err;
+                    fs.readdirSync(pathPrefix).forEach(verifyFolder)
+                });
+            }
+            else
+                setTimeout( myf,10)
+        }
+        setTimeout( myf,10)
+
     });
 
-    function insertTile(file)
+    function insertFolder(folder)
+    {
+        let stat = fs.lstatSync(pathPrefix+folder);
+        if (stat.isDirectory())
+            fs.readdirSync(pathPrefix+folder).forEach
+            (
+                (file)=>
+                {
+                    insertTile(pathPrefix+folder,file)
+                }
+            );
+    }
+    function insertTile(pathPrefix,file)
     {
         var coords = file.match(/^RASTER_msk_(\d+)_(\d+)_(\d+).png$/);
         if (!coords) return;
@@ -27,36 +62,46 @@ new MBTiles(__dirname+'/mbtile/tstfile.mbtiles?mode=rwc', function(err, mbtiles)
         // Flip Y coordinate because file names are TMS, but .putTile() expects XYZ.
         // coords[2] = Math.pow(2, coords[3]) - 1 - coords[2];
 
-        var tile = fs.readFileSync(__dirname + '/RASTER_msk_/15/' + file);
+        var tile = fs.readFileSync(pathPrefix+'/' + file);
+        completed.startFiles++;
         // mbtiles.putTile(coords[3] | 0, coords[1] | 0, coords[2] | 0,
         mbtiles.putTile(coords[1] | 0, coords[2] | 0, coords[3] | 0,            tile,
 
                 function (err) {
             if (err) throw err;
             completed.written++;
-            if (completed.written === 6) {
-                mbtiles.stopWriting(function (err)
+        });
+    }
+
+    function verifyFolder(folder)
+    {
+        let stat = fs.lstatSync(pathPrefix+folder);
+        if (stat.isDirectory())
+            fs.readdirSync(pathPrefix+folder).forEach
+            (
+                (file)=>
                 {
-                    completed.stopped = true;
-                    if (err) throw err;
-                    // verifyWritten();
-                });
-            }
-        });
+                    verifyTile(pathPrefix+folder,file)
+                }
+            );
     }
-    function verifyWritten() {
-        fs.readdirSync(__dirname + '/RASTER_msk_/15/').forEach(function(file) {
-            var coords = file.match(/^RASTER_msk_(\d+)_(\d+)_(\d+).png$/);
-            if (coords) {
-                // Flip Y coordinate because file names are TMS, but .getTile() expects XYZ.
-                coords[2] = Math.pow(2, coords[3]) - 1 - coords[2];
-                mbtiles.getTile(coords[3] | 0, coords[1] | 0, coords[2] | 0, function(err, tile) {
-                    if (err) throw err;
-                    assert.deepEqual(tile, fs.readFileSync(__dirname + '/RASTER_msk_/15/' + file));
-                    completed.read++;
-                    if (completed.read === 6) assert.end();
-                });
-            }
-        });
+
+    function verifyTile(folder,file)
+    {
+        var coords = file.match(/^RASTER_msk_(\d+)_(\d+)_(\d+).png$/);
+        if (coords) {
+            // Flip Y coordinate because file names are TMS, but .getTile() expects XYZ.
+            // coords[2] = Math.pow(2, coords[3]) - 1 - coords[2];
+            // mbtiles.getTile(coords[3] | 0, coords[1] | 0, coords[2] | 0, function(err, tile) {
+            mbtiles.getTile(coords[1] | 0, coords[2] | 0, coords[3] | 0,  function(err, tile) {
+                if (err) throw err;
+                assert.deepEqual(tile, fs.readFileSync(folder+'/' + file));
+                completed.read++;
+                if (completed.read === completed.startFiles)
+                    // assert.end();
+                    console.log("Verify success")
+            });
+        }
     }
+
 });
